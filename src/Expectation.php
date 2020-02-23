1e0eaeea366ca9e836f05040b5174e6fbcb3d0da
Tijmen Wierenga <t.wierenga@live.nl>
2020-02-23 19:40:13 +0100

    Implemented maximum invocations for an expectation

--- src/Expectation.php
+++ src/Expectation.php
@@ -12,11 +12,14 @@ final class Expectation
 {
     private Closure $condition;
     private ResponseInterface $response;
+    private int $maxInvocations;
+    private int $invocations = 0;
 
-    public function __construct(callable $condition, ResponseInterface $response)
+    public function __construct(callable $condition, ResponseInterface $response, int $maxInvocations = PHP_INT_MAX)
     {
         $this->condition = Closure::fromCallable($condition);
         $this->response = $response;
+        $this->maxInvocations = $maxInvocations;
     }
 
     public function getResponse(): ResponseInterface
@@ -24,8 +27,29 @@ final class Expectation
         return $this->response;
     }
 
+    /**
+     * @internal
+     * @psalm-internal TijmenWierenga\Guzzle\Mocking\MockHandler
+     */
+    public function addInvocation(): void
+    {
+        $this->invocations++;
+    }
+
     public function matches(RequestInterface $request): bool
     {
+        if ($this->reachedMaxInvocations()) {
+            return false;
+        }
+
         return (bool) ($this->condition)($request);
+    }
+
+    /**
+     * @psalm-mutation-free
+     */
+    private function reachedMaxInvocations(): bool
+    {
+        return $this->invocations >= $this->maxInvocations;
     }
 }
